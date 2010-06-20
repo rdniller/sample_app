7956bbc41fc950534111e006b50515c8a468e017
Russell Niller IV <russ@russell-niller-ivs-macbook-pro-2.local>
2010-06-20 19:29:15 -0400

    added static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
-
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
+  integrate_views
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -21,4 +16,29 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+  describe "should have the right page title" do
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial Sample App | Contact")
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial Sample App | About")
+    end    
 end
