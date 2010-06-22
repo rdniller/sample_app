c631578034b8905750f9cceef340af12d1f1e452
Russell Niller IV <russ@russell-niller-ivs-macbook-pro-2.local>
2010-06-22 00:29:57 -0400

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about '/about', :controller => 'pages', :action => 'about'
   map.help '/help', :controller => 'pages', :action => 'help'
+  map.signup '/signup', :controller => 'users', :action => 'new'
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
@@ -35,7 +36,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => 'pages', :action => 'home'
-
+  
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
